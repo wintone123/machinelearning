e79032ba10b393e48e3280039b36ca4276c2c2d2
民用罐头 <ragnarokduantech@gmaill.com>
2019-05-08 10:21:11 +0200

    1

--- hm.R
+++ hm.R
@@ -74,7 +74,7 @@ TileSequence <- function(seqname, start, end, tilewidth){
 HM_list <- c("h3k4me3", "h3k4me1", "h3k9me3", "h3k36me3")
 data_path <- "/mnt/c/chipseq/test8"
 anno_path <- "/mnt/c/others/anno_chr1.csv"
-output_path <- "/mnt/c/machinelearning/test1/gene_image"
+output_path <- "/mnt/c/machinelearning/test1"
 TSS_up <- 2000
 TSS_down <- 2000
 TTS_up <- 2000
@@ -94,56 +94,106 @@ anno_temp <- readr::read_delim(anno_path, delim = ",", col_names = TRUE)
 
 # anno arrange
 anno <- filter(anno_temp, biotype %in% c("protein_coding") & abs(TTS - TSS) > TSS_down + TTS_up)
-# anno <- data.frame(gene_name = anno_temp$gene_name,
-#                    TSS = ifelse(anno_temp$strand == "+", anno_temp$TSS, anno_temp$TTS),
-#                    TTS = ifelse(anno_temp$strand == "+", anno_temp$TTS, anno_temp$TSS))
-anno$TSS_up <- anno$TSS - TSS_up
-anno$TSS_down <- anno$TSS + TSS_down
-anno$TTS_up <- anno$TTS - TTS_up
-anno$TTS_down <- anno$TTS + TTS_down
-gene_list <- unique(anno$gene_name)
+anno_po <- filter(anno, strand == "+")
+anno_po$TSS_up <- anno_po$TSS - TSS_up
+anno_po$TSS_down <- anno_po$TSS + TSS_down
+anno_po$TTS_up <- anno_po$TTS - TTS_up
+anno_po$TTS_down <- anno_po$TTS + TTS_down
+gene_po_list <- unique(anno_po$gene_name)
+anno_ne <- filter(anno, strand == "-")
+anno_ne$TSS_up <- anno_ne$TTS + TSS_up
+anno_ne$TSS_down <- anno_ne$TTS - TSS_down
+anno_ne$TTS_up <- anno_ne$TSS + TTS_up
+anno_ne$TTS_down <- anno_ne$TSS - TTS_down
+gene_ne_list <- unique(anno_ne$gene_name)
 
 # procession
-cat("-------------processing-------------", "\n")
-# cl <- makeCluster(2)
-# clusterExport(cl, c("anno", "binsize", "output_path", "h3k4me3", "h3k4me3_in", 
-#                     "TileSequence", "mapping", "BinChipseq"), envir = globalenv())
-# clusterEvalQ(cl, "GenomicRanges")
-# parLapply(cl, gene_list, binning)
-# stopCluster(cl)
-HM_temp_list <- paste0(HM_list, "_temp")
-for (i in 1:length(gene_list)) {
-    gene <- anno[anno$gene_name == gene_list[i],][1,]
+cat("---------------binning---------------", "\n")
+# positive strand
+j <- 0
+k <- 0
+for (i in 1:length(gene_po_list)) {
+    gene <- anno_po[anno_po$gene_name == gene_po_list[i],][1,]
     tss_bins <- TileSequence("1", gene$TSS_up, gene$TSS_down, binsize)
     tts_bins <- TileSequence("1", gene$TTS_up, gene$TTS_down, binsize)
     inter_bins <- GRanges(seqnames = Rle("1"),
                           ranges = IRanges(start = gene$TSS_down +1, end = gene$TTS_up - 1),
                           strand = Rle("*"))
     gene_bins <- Reduce("c", list(tss_bins, inter_bins, tts_bins))
-    gene_bins$name <- rep(gene_list[i], length(gene_bins))
+    gene_bins$name <- rep(gene_po_list[i], length(gene_bins))
     if (i == 1) {
-        out_bins <- gene_bins
+        out_po_bins <- gene_bins
     } else {
-        out_bins <- Reduce("c", list(out_bins, gene_bins))
+        out_po_bins <- Reduce("c", list(out_po_bins, gene_bins))
     }
+
+    # progress bar
+    if ((j / (length(gene_ne_list) + length(gene_po_list)) * 100) %/% 1 == k) { 
+        cat("[", paste(rep("#", (30*k/100) %/% 1),collapse = ""), 
+        paste(rep("_", 30-(30*k/100) %/% 1),collapse = ""), k, "%]","\r", sep = "")
+        k <- k + 1
+    }
+    j <- j + 1 
 }
+# negative strand
+for (i in 1:length(gene_ne_list)) {
+    gene <- anno_ne[anno_ne$gene_name == gene_ne_list[i],][1,]
+    tss_bins <- TileSequence("1", gene$TSS_down, gene$TSS_up, binsize)
+    tts_bins <- TileSequence("1", gene$TTS_down, gene$TTS_up, binsize)
+    inter_bins <- GRanges(seqnames = Rle("1"),
+                          ranges = IRanges(start = gene$TTS_up + 1, end = gene$TSS_down - 1),
+                          strand = Rle("*"))
+    gene_bins <- Reduce("c", list(tts_bins, inter_bins, tss_bins))
+    gene_bins$name <- rep(gene_ne_list[i], length(gene_bins))
+    if (i == 1) {
+        out_ne_bins <- gene_bins
+    } else {
+        out_ne_bins <- Reduce("c", list(out_ne_bins, gene_bins))
+    }
+
+    # progress bar
+    if ((j / (length(gene_ne_list) + length(gene_po_list)) * 100) %/% 1 == k) { 
+        cat("[", paste(rep("#", (30*k/100) %/% 1),collapse = ""), 
+        paste(rep("_", 30-(30*k/100) %/% 1),collapse = ""), k, "%]","\r", sep = "")
+        k <- k + 1
+    }
+    j <- j + 1 
+}
+
+HM_po_temp_list <- paste0(HM_list, "_po_temp")
 for (name in HM_list) {
-    assign(paste0(name, "_temp"), BinChipseq(eval(parse(text = name)), out_bins))
+    assign(paste0(name, "_po_temp"), BinChipseq(eval(parse(text = name)), out_po_bins))
 }
+HM_ne_temp_list <- paste0(HM_list, "_ne_temp")
+for (name in HM_list) {
+    assign(paste0(name, "_ne_temp"), BinChipseq(eval(parse(text = name)), out_ne_bins))
+}
+
+cat("-------------overlapping-------------", "\n")
+# positive
 for (i in 1:length(HM_list)) {
-    out_df_temp <- data.frame(t(matrix(eval(parse(text = HM_temp_list[i]))$score, nrow = 81)))
+    out_df_temp <- data.frame(t(matrix(eval(parse(text = HM_po_temp_list[i]))$score, nrow = 81)))
     out_df_temp <- cbind(HM = rep(HM_list[i], nrow(out_df_temp)), out_df_temp)
-    out_df_temp <- cbind(gene = gene_list, out_df_temp)
+    out_df_temp <- cbind(gene = gene_po_list, out_df_temp)
     if (i == 1) {
         out_df <- out_df_temp
     } else {
         out_df <- rbind(out_df, out_df_temp)
-    }    
+    } 
 }
+# negative
+for (i in 1:length(HM_list)) {
+    out_df_temp <- data.frame(t(matrix(eval(parse(text = HM_ne_temp_list[i]))$score, nrow = 81)))
+    out_df_temp <- out_df_temp[,ncol(out_df_temp):1]
+    out_df_temp <- cbind(HM = rep(HM_list[i], nrow(out_df_temp)), out_df_temp)
+    out_df_temp <- cbind(gene = gene_ne_list, out_df_temp)
+    out_df <- rbind(out_df, out_df_temp)
+}
+
 colnames(out_df)[1] <- "gene" 
 colnames(out_df)[2] <- "HM" 
 out_df <- arrange(out_df, gene, HM)
 
 # output
-cat("------------writing csv-------------", "\n")
+cat("-------------writing csv-------------", "\n")
 write.csv(out_df, file.path(output_path, "output.csv"), row.names = FALSE)
---
+++ image.R
@@ -0,0 +1,65 @@
+# library
+library(dplyr)
+library(tidyr)
+library(pheatmap)
+library(RColorBrewer)
+library(parallel)
+
+# functions
+max_legend <- function(a) {
+    cond <- TRUE
+    n <- 0
+    while (cond) {
+        n <- n + 1
+        b = a %/% 2
+        if (b == 1) {
+            cond <- FALSE
+            return(n + 1)
+        } else {
+            a <- b
+        }
+    }
+}
+
+# imaging <- function(mt) {
+#     pheatmap(mt, cluster_rows = FALSE, cluster_cols = FALSE,
+#              show_rownames = FALSE, show_colnames = FALSE, 
+#              col = col(length(breaks)), breaks = breaks, legend = FALSE, 
+#              border_color = NA,
+#              filename = paste0(path_out, "/", gene_name, ".png"))
+# }
+
+imaging_par <- function(gene_name) {
+    temp_mt <- as.matrix(filter(imput, gene == gene_name)[,3:83])
+    if (sum(temp_mt) != 0) {
+        pheatmap(temp_mt, cluster_rows = FALSE, cluster_cols = FALSE,
+                 show_rownames = FALSE, show_colnames = FALSE, 
+                 col = col(length(breaks)), breaks = breaks, legend = FALSE, 
+                 border_color = NA,
+                 filename = paste0(path_out, "/", gene_name, ".png"))
+    }
+}
+
+# parameter
+path_in <- "/mnt/c/machinelearning/test1/output2.csv"
+path_out <- "/mnt/c/machinelearning/test1/gene_image_2"
+col <- colorRampPalette(brewer.pal(9,"YlOrRd"))
+
+# load file
+cat("-------------loading csv-------------", "\n")
+imput <- readr::read_csv(path_in, col_names = TRUE)
+imput_mt <- as.matrix(imput[,3:83])
+breaks <- seq(0, max_legend(max(imput_mt)))
+
+# process
+cat("--------------processing--------------", "\n")
+cl <- makeForkCluster(2)
+clusterExport(cl, c("col", "breaks", "imput"))
+parLapply(cl, unique(imput$gene), imaging_par)
+stopCluster(cl)
+# for (gene_name in unique(imput$gene)) {
+#     temp_mt <- as.matrix(filter(imput, gene == gene_name)[,3:83])
+#     if (sum(temp_mt) != 0) {
+#         imaging(temp_mt)
+#     }
+# }
